cea06e4f3aad856349e7b4557781a38437dc5e5f
iperov <lepersorium@gmail.com>
2022-11-08 19:13:24 +0400

    update onnxruntime

--- build/windows/WindowsBuilder.py
+++ build/windows/WindowsBuilder.py
@@ -485,9 +485,9 @@ def build_deepfacelive_windows(release_dir, cache_dir, python_ver='3.7.9', backe
         #builder.install_pip_package('torch==1.11.0+cu115 torchvision==0.12.0+cu115 -f https://download.pytorch.org/whl/torch_stable.html')
         builder.install_pip_package('torch==1.12.1+cu116 torchvision==0.13.1+cu116 -f https://download.pytorch.org/whl/torch_stable.html')
         
-        builder.install_pip_package('onnxruntime-gpu==1.11.0')
+        builder.install_pip_package('onnxruntime-gpu==1.13.1')
     elif backend == 'directml':
-        builder.install_pip_package('onnxruntime-directml==1.12.1')
+        builder.install_pip_package('onnxruntime-directml==1.13.1')
 
     builder.install_ffmpeg_binaries()
 
